959f9b5a7fa8dac5b5ccfb62e1151948e6bf9013
Oratory <Oratory89@gmail.com>
2022-07-08 22:02:55 +0200

    Stuff change

--- .bmx/game.bmx.gui.release.win32.x64.c
+++ .bmx/game.bmx.gui.release.win32.x64.c
@@ -23,14 +23,20 @@ static BBString _s1={
 	,97,100,121,32,101,120,105,115,116,115,32,105,110,32,116,104
 	,101,32,109,97,112}
 };
-static BBString _s14={
+static BBString _s13={
+	&bbStringClass,
+	0x74ea3849079de930,
+	14,
+	{68,101,102,97,117,108,116,95,87,101,97,112,111,110}
+};
+static BBString _s12={
 	&bbStringClass,
 	0xad36fdc9aaf2b5ac,
 	17,
 	{84,101,120,116,117,114,101,115,92,68,117,100,101,46,98,109,112
 }
 };
-static BBString _s13={
+static BBString _s11={
 	&bbStringClass,
 	0x36070fd1abc59a7e,
 	19,
@@ -50,20 +56,6 @@ static BBString _s3={
 	18,
 	{84,104,101,32,115,116,97,99,107,32,105,115,32,101,109,112,116
 	,121}
-};
-static BBString _s12={
-	&bbStringClass,
-	0xd1123a720161ad7,
-	18,
-	{92,84,101,120,116,117,114,101,115,92,68,117,100,101,46,98,109
-	,112}
-};
-static BBString _s11={
-	&bbStringClass,
-	0x2ff5169bcee4f667,
-	20,
-	{92,84,101,120,116,117,114,101,115,92,71,114,111,117,110,100,46
-	,98,109,112}
 };
 static BBString _s4={
 	&bbStringClass,
@@ -105,22 +97,234 @@ static BBString _s8={
 	5,
 	{111,116,104,101,114}
 };
+struct BBDebugScope_10{int kind; const char *name; BBDebugDecl decls[11]; };
+struct BBDebugScope_7{int kind; const char *name; BBDebugDecl decls[8]; };
 struct brl_max2d_image_TImage_obj* _m_game_GroundTImage=(struct brl_max2d_image_TImage_obj*)(&bbNullObject);
 struct brl_max2d_image_TImage_obj* _m_game_DudeTImage=(struct brl_max2d_image_TImage_obj*)(&bbNullObject);
 BBFLOAT _m_game_DudeX=400.000000f;
 BBFLOAT _m_game_DudeY=250.000000f;
+struct brl_linkedlist_TList_obj* _m_game_CharacterList=(struct brl_linkedlist_TList_obj*)(&bbNullObject);
+struct brl_linkedlist_TList_obj* _m_game_ProjectileList=(struct brl_linkedlist_TList_obj*)(&bbNullObject);
 BBARRAY _m_game_Map_Array=(&bbEmptyArray);
-extern const unsigned char * _ib_bb_main_1_data;
-extern const unsigned int _ib_bb_main_1_size;
-extern const unsigned char * _ib_bb_main_2_data;
-extern const unsigned int _ib_bb_main_2_size;
+void __m_game_CharacterType_New(struct _m_game_CharacterType_obj* o) {
+	bbObjectCtor((BBOBJECT)o);
+	o->clas = &_m_game_CharacterType;
+	o->__m_game_charactertype_x = .00000000f;
+	o->__m_game_charactertype_y = .00000000f;
+	o->__m_game_charactertype_dir = .00000000f;
+	o->__m_game_charactertype_speed = 0.02f;
+	o->__m_game_charactertype_texture = (struct brl_max2d_image_TImage_obj*)_m_game_DudeTImage;
+	o->__m_game_charactertype_weapon = &_s13;
+}
+void __m_game_CharacterType_Draw(struct _m_game_CharacterType_obj* o){
+	brl_max2d_DrawImage((struct brl_max2d_image_TImage_obj*)o->__m_game_charactertype_texture ,o->__m_game_charactertype_x ,o->__m_game_charactertype_y ,0);
+}
+void __m_game_CharacterType_Shoot(struct _m_game_CharacterType_obj* o){
+	struct _m_game_ProjectileType_obj* bbt_NewProjectile=(struct _m_game_ProjectileType_obj*)(&bbNullObject);
+	bbt_NewProjectile=(struct _m_game_ProjectileType_obj*)(struct _m_game_ProjectileType_obj*)bbObjectAtomicNew((BBClass *)&_m_game_ProjectileType);
+	bbt_NewProjectile->__m_game_projectiletype_x =o->__m_game_charactertype_x ;
+	bbt_NewProjectile->__m_game_projectiletype_y =o->__m_game_charactertype_y ;
+	bbt_NewProjectile->__m_game_projectiletype_dir =o->__m_game_charactertype_dir ;
+	bbt_NewProjectile->__m_game_projectiletype_speed =0.02f;
+	(_m_game_ProjectileList)->clas->m_AddLast_TObject((struct brl_linkedlist_TList_obj*)_m_game_ProjectileList,(BBOBJECT)bbt_NewProjectile);
+}
+void __m_game_CharacterType_Move(struct _m_game_CharacterType_obj* o){
+	if(o->__m_game_charactertype_speed <0.000000000f){
+		o->__m_game_charactertype_speed =0.000000000f;
+	}
+	if(o->__m_game_charactertype_speed >0.1f){
+		o->__m_game_charactertype_speed =0.1f;
+	}
+	if(brl_polledinput_KeyDown(68)!=0){
+		o->__m_game_charactertype_dir =(o->__m_game_charactertype_dir +0.1f);
+	}
+	if(brl_polledinput_KeyDown(65)!=0){
+		o->__m_game_charactertype_dir =(o->__m_game_charactertype_dir -0.1f);
+	}
+	if(brl_polledinput_KeyDown(87)!=0){
+		o->__m_game_charactertype_speed =(o->__m_game_charactertype_speed +0.0001f);
+	}
+	if(brl_polledinput_KeyDown(83)!=0){
+		o->__m_game_charactertype_speed =(o->__m_game_charactertype_speed -0.0001f);
+	}
+	if(brl_polledinput_KeyDown(32)!=0){
+		((struct _m_game_CharacterType_obj*)o)->clas->m_Shoot((struct _m_game_CharacterType_obj*)o);
+	}
+	o->__m_game_charactertype_x =((BBFLOAT)(((BBDOUBLE)o->__m_game_charactertype_x )+(((BBDOUBLE)o->__m_game_charactertype_speed )*bbCos((double)((BBDOUBLE)o->__m_game_charactertype_dir )))));
+	o->__m_game_charactertype_y =((BBFLOAT)(((BBDOUBLE)o->__m_game_charactertype_y )+(((BBDOUBLE)o->__m_game_charactertype_speed )*bbSin((double)((BBDOUBLE)o->__m_game_charactertype_dir )))));
+}
+struct BBDebugScope_10 _m_game_CharacterType_scope ={
+	BBDEBUGSCOPE_USERTYPE,
+	"CharacterType",
+	{
+		{
+			BBDEBUGDECL_FIELD,
+			"X",
+			"f",
+			.field_offset=offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_x)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"Y",
+			"f",
+			.field_offset=offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_y)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"Dir",
+			"f",
+			.field_offset=offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_dir)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"Speed",
+			"f",
+			.field_offset=offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_speed)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"Texture",
+			":TImage",
+			.field_offset=offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_texture)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"Weapon",
+			"$",
+			.field_offset=offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_weapon)
+		},
+		{
+			BBDEBUGDECL_TYPEMETHOD,
+			"New",
+			"()",
+			.var_address=(void*)&__m_game_CharacterType_New
+		},
+		{
+			BBDEBUGDECL_TYPEMETHOD,
+			"Draw",
+			"()",
+			.var_address=(void*)&__m_game_CharacterType_Draw
+		},
+		{
+			BBDEBUGDECL_TYPEMETHOD,
+			"Shoot",
+			"()",
+			.var_address=(void*)&__m_game_CharacterType_Shoot
+		},
+		{
+			BBDEBUGDECL_TYPEMETHOD,
+			"Move",
+			"()",
+			.var_address=(void*)&__m_game_CharacterType_Move
+		},
+		BBDEBUGDECL_END
+	}
+};
+struct BBClass__m_game_CharacterType _m_game_CharacterType={
+	&bbObjectClass,
+	bbObjectFree,
+	(BBDebugScope*)&_m_game_CharacterType_scope,
+	sizeof(struct _m_game_CharacterType_obj),
+	(void (*)(BBOBJECT))__m_game_CharacterType_New,
+	bbObjectDtor,
+	bbObjectToString,
+	bbObjectCompare,
+	bbObjectSendMessage,
+	0,
+	0,
+	offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_weapon) - offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_x) + sizeof(BBSTRING)
+	,0
+	,offsetof(struct _m_game_CharacterType_obj,__m_game_charactertype_x)
+	,__m_game_CharacterType_Draw
+	,__m_game_CharacterType_Shoot
+	,__m_game_CharacterType_Move
+};
+
+void __m_game_ProjectileType_New(struct _m_game_ProjectileType_obj* o) {
+	bbObjectCtor((BBOBJECT)o);
+	o->clas = &_m_game_ProjectileType;
+	o->__m_game_projectiletype_id = 0;
+	o->__m_game_projectiletype_x = .00000000f;
+	o->__m_game_projectiletype_y = .00000000f;
+	o->__m_game_projectiletype_dir = .00000000f;
+	o->__m_game_projectiletype_speed = 0.2f;
+}
+void __m_game_ProjectileType_Move(struct _m_game_ProjectileType_obj* o){
+	o->__m_game_projectiletype_x =((BBFLOAT)(((BBDOUBLE)o->__m_game_projectiletype_x )+(((BBDOUBLE)o->__m_game_projectiletype_speed )*bbCos((double)((BBDOUBLE)o->__m_game_projectiletype_dir )))));
+	o->__m_game_projectiletype_y =((BBFLOAT)(((BBDOUBLE)o->__m_game_projectiletype_y )+(((BBDOUBLE)o->__m_game_projectiletype_speed )*bbSin((double)((BBDOUBLE)o->__m_game_projectiletype_dir )))));
+}
+struct BBDebugScope_7 _m_game_ProjectileType_scope ={
+	BBDEBUGSCOPE_USERTYPE,
+	"ProjectileType",
+	{
+		{
+			BBDEBUGDECL_FIELD,
+			"ID",
+			"i",
+			.field_offset=offsetof(struct _m_game_ProjectileType_obj,__m_game_projectiletype_id)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"X",
+			"f",
+			.field_offset=offsetof(struct _m_game_ProjectileType_obj,__m_game_projectiletype_x)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"Y",
+			"f",
+			.field_offset=offsetof(struct _m_game_ProjectileType_obj,__m_game_projectiletype_y)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"Dir",
+			"f",
+			.field_offset=offsetof(struct _m_game_ProjectileType_obj,__m_game_projectiletype_dir)
+		},
+		{
+			BBDEBUGDECL_FIELD,
+			"Speed",
+			"f",
+			.field_offset=offsetof(struct _m_game_ProjectileType_obj,__m_game_projectiletype_speed)
+		},
+		{
+			BBDEBUGDECL_TYPEMETHOD,
+			"New",
+			"()",
+			.var_address=(void*)&__m_game_ProjectileType_New
+		},
+		{
+			BBDEBUGDECL_TYPEMETHOD,
+			"Move",
+			"()",
+			.var_address=(void*)&__m_game_ProjectileType_Move
+		},
+		BBDEBUGDECL_END
+	}
+};
+struct BBClass__m_game_ProjectileType _m_game_ProjectileType={
+	&bbObjectClass,
+	bbObjectFree,
+	(BBDebugScope*)&_m_game_ProjectileType_scope,
+	sizeof(struct _m_game_ProjectileType_obj),
+	(void (*)(BBOBJECT))__m_game_ProjectileType_New,
+	bbObjectDtor,
+	bbObjectToString,
+	bbObjectCompare,
+	bbObjectSendMessage,
+	0,
+	0,
+	offsetof(struct _m_game_ProjectileType_obj,__m_game_projectiletype_speed) - offsetof(struct _m_game_ProjectileType_obj,__m_game_projectiletype_id) + sizeof(BBFLOAT)
+	,0
+	,offsetof(struct _m_game_ProjectileType_obj,__m_game_projectiletype_id)
+	,__m_game_ProjectileType_Move
+};
+
 static int _bb_main_inited = 0;
 int _bb_main(){
 	if (!_bb_main_inited) {
 		_bb_main_inited = 1;
 		GC_add_roots(&_m_game_GroundTImage, &_m_game_Map_Array + 1);
-		bbIncbinAdd(&_s11,&_ib_bb_main_1_data,_ib_bb_main_1_size);
-		bbIncbinAdd(&_s12,&_ib_bb_main_2_data,_ib_bb_main_2_size);
 		__bb_brl_blitz_blitz();
 		__bb_brl_appstub_appstub();
 		__bb_brl_audio_audio();
@@ -171,10 +375,14 @@ int _bb_main(){
 		__bb_pub_opengles3_opengles3();
 		__bb_pub_vulkan_vulkan();
 		__bb_pub_xmmintrin_xmmintrin();
+		bbObjectRegisterType((BBCLASS)&_m_game_CharacterType);
+		bbObjectRegisterType((BBCLASS)&_m_game_ProjectileType);
 		brl_graphics_Graphics(800,600,0,0,0,-1,-1);
 		brl_random_SeedRnd(bbMilliSecs());
-		_m_game_GroundTImage=(struct brl_max2d_image_TImage_obj*)brl_max2d_LoadImage((BBOBJECT)&_s13,-1);
-		_m_game_DudeTImage=(struct brl_max2d_image_TImage_obj*)brl_max2d_LoadImage((BBOBJECT)&_s14,-1);
+		_m_game_GroundTImage=(struct brl_max2d_image_TImage_obj*)brl_max2d_LoadImage((BBOBJECT)&_s11,-1);
+		_m_game_DudeTImage=(struct brl_max2d_image_TImage_obj*)brl_max2d_LoadImage((BBOBJECT)&_s12,-1);
+		_m_game_CharacterList=(struct brl_linkedlist_TList_obj*)brl_linkedlist_CreateList();
+		_m_game_ProjectileList=(struct brl_linkedlist_TList_obj*)brl_linkedlist_CreateList();
 		_m_game_Map_Array=bbArrayNew("i", 2, 50, 50);
 		{
 			BBINT bbt_X=0;
@@ -183,19 +391,52 @@ int _bb_main(){
 				((BBINT*)BBARRAYDATA(_m_game_Map_Array,1))[(*(bbt_)) * ((BBUINT)bbt_X) + 37U]=1;
 			}
 		}
+		{
+			BBINT bbt_N=1;
+			for(;(bbt_N<=1);bbt_N=(bbt_N+1)){
+				struct _m_game_CharacterType_obj* bbt_NewCharacter=(struct _m_game_CharacterType_obj*)(&bbNullObject);
+				bbt_NewCharacter=(struct _m_game_CharacterType_obj*)(struct _m_game_CharacterType_obj*)bbObjectNew((BBClass *)&_m_game_CharacterType);
+				bbt_NewCharacter->__m_game_charactertype_x =400.000000f;
+				bbt_NewCharacter->__m_game_charactertype_y =300.000000f;
+				bbt_NewCharacter->__m_game_charactertype_dir =((BBFLOAT)brl_random_Rand(0,360));
+				(_m_game_CharacterList)->clas->m_AddLast_TObject((struct brl_linkedlist_TList_obj*)_m_game_CharacterList,(BBOBJECT)bbt_NewCharacter);
+			}
+		}
 		while(!(brl_polledinput_KeyDown(27)!=0)){
 			brl_max2d_Cls();
-			brl_max2d_DrawImage((struct brl_max2d_image_TImage_obj*)_m_game_DudeTImage,(_m_game_DudeX-8.00000000f),(_m_game_DudeY-32.0000000f),0);
+			struct brl_linkedlist_TListEnum_obj* bbt_2=(struct brl_linkedlist_TListEnum_obj*)(_m_game_CharacterList)->clas->m_ObjectEnumerator((struct brl_linkedlist_TList_obj*)_m_game_CharacterList);
+			while((bbt_2)->clas->m_HasNext((struct brl_linkedlist_TListEnum_obj*)bbt_2)!=0){
+				struct _m_game_CharacterType_obj* bbt_Character=(struct _m_game_CharacterType_obj*)((struct _m_game_CharacterType_obj*)bbObjectDowncast((BBOBJECT)(bbt_2)->clas->m_NextObject((struct brl_linkedlist_TListEnum_obj*)bbt_2),(BBClass*)&_m_game_CharacterType));
+				if(bbt_Character==&bbNullObject){
+					continue;
+				}
+				(bbt_Character)->clas->m_Draw((struct _m_game_CharacterType_obj*)bbt_Character);
+				(bbt_Character)->clas->m_Move((struct _m_game_CharacterType_obj*)bbt_Character);
+				brl_standardio_Print(bbStringFromFloat(bbt_Character->__m_game_charactertype_dir ));
+				brl_standardio_Print(bbStringFromFloat(bbt_Character->__m_game_charactertype_speed ));
+			}
 			{
 				BBINT bbt_X2=0;
 				for(;(bbt_X2<=49);bbt_X2=(bbt_X2+1)){
 					{
 						BBINT bbt_Y=0;
 						for(;(bbt_Y<=49);bbt_Y=(bbt_Y+1)){
-							BBUINT* bbt_2=((BBARRAY)_m_game_Map_Array)->scales + 1;
-							if(((BBINT*)BBARRAYDATA(_m_game_Map_Array,1))[(*(bbt_2)) * ((BBUINT)bbt_X2) + ((BBUINT)bbt_Y)]==1){
+							BBUINT* bbt_3=((BBARRAY)_m_game_Map_Array)->scales + 1;
+							if(((BBINT*)BBARRAYDATA(_m_game_Map_Array,1))[(*(bbt_3)) * ((BBUINT)bbt_X2) + ((BBUINT)bbt_Y)]==1){
 								brl_max2d_DrawImage((struct brl_max2d_image_TImage_obj*)_m_game_GroundTImage,((BBFLOAT)(bbt_X2*16)),((BBFLOAT)(bbt_Y*16)),0);
 							}
+						}
+					}
+					struct brl_linkedlist_TListEnum_obj* bbt_4=(struct brl_linkedlist_TListEnum_obj*)(_m_game_ProjectileList)->clas->m_ObjectEnumerator((struct brl_linkedlist_TList_obj*)_m_game_ProjectileList);
+					while((bbt_4)->clas->m_HasNext((struct brl_linkedlist_TListEnum_obj*)bbt_4)!=0){
+						struct _m_game_ProjectileType_obj* bbt_Projectile=(struct _m_game_ProjectileType_obj*)((struct _m_game_ProjectileType_obj*)bbObjectDowncast((BBOBJECT)(bbt_4)->clas->m_NextObject((struct brl_linkedlist_TListEnum_obj*)bbt_4),(BBClass*)&_m_game_ProjectileType));
+						if(bbt_Projectile==&bbNullObject){
+							continue;
+						}
+						brl_max2d_DrawRect(bbt_Projectile->__m_game_projectiletype_x ,bbt_Projectile->__m_game_projectiletype_y ,1.00000000f,1.00000000f);
+						(bbt_Projectile)->clas->m_Move((struct _m_game_ProjectileType_obj*)bbt_Projectile);
+						if((bbt_Projectile->__m_game_projectiletype_x >800.000000f) || (bbt_Projectile->__m_game_projectiletype_x <0.000000000f)){
+							brl_linkedlist_ListRemove((struct brl_linkedlist_TList_obj*)_m_game_ProjectileList,(BBOBJECT)bbt_Projectile);
 						}
 					}
 				}
